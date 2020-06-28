e6ba94b6987d41c2913cb2319fcca32054ffb519
jwpark97114 <jwpark97114@gmail.com>
2020-06-28 03:15:16 -0700

    getView함수 오버라이딩 => 우리가 만든 xml이 ListView에 나올 수 있도록 작업

--- app/src/main/java/kr/co/namu/listview_20200628/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/namu/listview_20200628/adapters/StudentAdapter.kt
@@ -1,8 +1,33 @@
 package kr.co.namu.listview_20200628.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kr.co.namu.listview_20200628.R
 import kr.co.namu.listview_20200628.datas.Student
 
 class StudentAdapter(val mContext:Context, val resId:Int, val mList:ArrayList<Student>): ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+//    한줄씩 직접 그려두는 함수
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+//        convertView : 기존에 그려둔 줄이 있는지(재활용 할게 있는지) 판단하는 재료
+        var tempRow = convertView
+
+//    재활용 할 view가 없는가? 없다면 xml을 새로 그려서 tempRow에 대입
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+//    재활용 여부와 관계없이 이제 무조건 tempRow안에 무엇인가 들어있다.
+
+//    row에는 절대 null이 있을 리가 없다고 말하며 대입
+    val row = tempRow!!
+
+    return row
+    }
 }
